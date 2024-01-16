0e57e82061dca679d18d508c667811d6ba529749
Rogênio Belém <rogeniobelem@gmail.com>
2024-01-16 18:01:15 -0300

    Adicionando componente slider

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # review_amazon_livros
 
 Review dos 100 livros mais vendidos da Amazon
+
+# Requirements
+
++ streamlit
++ plotly
++ 
--- review_livros.py
+++ review_livros.py
@@ -6,4 +6,11 @@ st.set_page_config(layout="wide")
 df_reviews = pd.read_csv("datasets/customer reviews.csv")
 df_top_100_books = pd.read_csv("datasets/Top-100 Trending Books.csv")
 
-df_reviews
+preco_max = df_top_100_books["book price"].max()
+preco_min = df_top_100_books["book price"].min()
+
+preco_ate = st.sidebar.slider(
+    "Faixa de preço", preco_min, preco_max, preco_max)
+
+df_books = df_top_100_books[df_top_100_books["book price"] <= preco_ate]
+st.write(df_books)
